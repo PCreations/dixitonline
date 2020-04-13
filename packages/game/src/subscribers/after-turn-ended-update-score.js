f345b157b274dcb4fa6c0dcbe793399fef801fd6
Pierre Criulanscy <pcriulan@gmail.com>
2020-04-13 14:20:52 +0200

    fix(game): fix update score

--- packages/game/src/subscribers/after-turn-ended-update-score.js
+++ packages/game/src/subscribers/after-turn-ended-update-score.js
@@ -1,9 +1,7 @@
 import { events as turnEvents } from '@dixit/turn';
 
 export const makeAfterTurnEndedUpdateScoreSubscriber = ({ subscribeToDomainEvent, updateScore }) => {
-  console.log('Subsribing to TURN_ENDED in game (update score)');
   subscribeToDomainEvent(turnEvents.types.TURN_ENDED, async turnEndedEvent => {
-    console.log(`[TURN_ENDED] received in game after turn ended update score`);
     const { gameId, playersWithHandAndScore } = turnEndedEvent.payload;
     const turnScore = playersWithHandAndScore.reduce(
       (scores, { playerId, score }) => ({
