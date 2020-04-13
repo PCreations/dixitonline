c4785a4d7b624ec4db99712cbe2eb9186db361d0
Pierre Criulanscy <pcriulan@gmail.com>
2020-04-13 14:20:11 +0200

    feat(deck): deck is now a simple array of card

--- packages/decks/src/useCases/__tests__/get-shuffled-deck.test.js
+++ packages/decks/src/useCases/__tests__/get-shuffled-deck.test.js
@@ -1,27 +1,20 @@
 import { shuffle as shuffleWithSeed } from 'shuffle-seed';
 import { events as deckEvents } from '../../events';
-import { makeNullDeckRepository } from '../../repos';
 import { makeGetShuffledDeck } from '../get-shuffled-deck';
 
 describe('get shuffled deck', () => {
-  it('retrieves the deck shuffled', async () => {
+  it('returns 84 card name where the card number is between 1 and 152', async () => {
     // arrange
-    const cards = [
-      { id: 'c1', url: 'http://example.com/c1.png' },
-      { id: 'c1', url: 'http://example.com/c1.png' },
-      { id: 'c1', url: 'http://example.com/c1.png' },
-    ];
     const shuffle = toShuffle => shuffleWithSeed(toShuffle, 'seed');
-    const expectedShuffledCards = shuffle(cards);
-    const deckRepository = makeNullDeckRepository({ defaultDeckCards: cards });
     const dispatchDomainEvents = jest.fn();
-    const getShuffledDeck = makeGetShuffledDeck({ deckRepository, shuffle, dispatchDomainEvents });
+    const getShuffledDeck = makeGetShuffledDeck({ shuffle, dispatchDomainEvents });
 
     // act
     const shuffledDeck = await getShuffledDeck({ gameId: 'g1' });
 
     // assert
-    expect(shuffledDeck.cards).toEqual(expectedShuffledCards);
+    expect(shuffledDeck.cards.length).toEqual(84);
+    expect(shuffledDeck.cards.every(cardNumber => cardNumber >= 1 && cardNumber <= 152));
     expect(dispatchDomainEvents).toHaveBeenCalledWith([
       deckEvents.deckShuffled({ gameId: 'g1', cards: shuffledDeck.cards }),
     ]);
--- packages/decks/src/useCases/get-shuffled-deck.js
+++ packages/decks/src/useCases/get-shuffled-deck.js
@@ -1,8 +1,11 @@
 import { events } from '../events';
 
-export const makeGetShuffledDeck = ({ deckRepository, shuffle, dispatchDomainEvents }) => async ({ gameId }) => {
-  const deck = await deckRepository.getDefaultDeck();
-  const cards = shuffle(deck.cards);
+const CARDS_IN_DECK = 152;
+const NUMBER_OF_CARDS_FOR_GAME_DECK = 84;
+
+export const makeGetShuffledDeck = ({ shuffle, dispatchDomainEvents }) => async ({ gameId }) => {
+  const deck = new Array(CARDS_IN_DECK).fill().map((_, index) => index + 1);
+  const cards = shuffle(deck).slice(0, NUMBER_OF_CARDS_FOR_GAME_DECK);
   dispatchDomainEvents([events.deckShuffled({ gameId, cards })]);
   return {
     name: deck.name,
