bea1b3aa0bb27b3670d0bcbb124219f12c084b20
Pierre Criulanscy <pcriulan@gmail.com>
2020-04-14 09:49:59 +0200

    add test security for turn phase to investigate issue

--- packages/turn/src/infra/graphql/schema/query.js
+++ packages/turn/src/infra/graphql/schema/query.js
@@ -8,6 +8,9 @@ export const GetTurnPhase = queryField('getTurnPhase', {
     turnId: idArg({ required: true }),
   },
   async resolve(_, { turnId }, { dataSources, currentUser }) {
+    if (!currentUser) {
+      throw new Error('unauthorized');
+    }
     const getTurnPhase = makeGetTurnPhase({ turnRepository: dataSources.turnRepository });
     const turn = await getTurnPhase({ turnId, playerId: currentUser.id });
     return mapPhaseStateToGraphQL(turn);
