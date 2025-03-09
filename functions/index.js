474314320184fae3e5c8403aa49efc758728faaa
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2025-03-09 18:40:21 +0100

    feat: use cloud pubsub instead of eventemitter

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,37 @@
-const { EventEmitter } = require('events');
-
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const { PubSub } = require('@google-cloud/pubsub');
 const serviceAccount = require('./secrets/dixit-firebase-admin.json');
 const { default: dixit } = require('./build/dixit');
+
+const TOPIC_NAME = 'dixit_game_events';
+
+const pubsub = new PubSub();
+
+const domainEventSubscribers = {};
 
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://dixit-af060.firebaseio.com',
 });
 
-const eventEmitter = new EventEmitter();
-
-const dispatchDomainEvents = events =>
-  events.map(event => {
-    eventEmitter.emit(event.type, event);
+// Fonction pour publier les événements
+const dispatchDomainEvents = events => {
+  events.forEach(async event => {
+    const dataBuffer = Buffer.from(JSON.stringify(event));
+    try {
+      await pubsub.topic(TOPIC_NAME).publish(dataBuffer);
+    } catch (err) {
+      console.error('Erreur lors de la publication de l’événement:', err);
+    }
   });
+};
 
 const subscribeToDomainEvent = (type, callback) => {
-  eventEmitter.on(type, event => {
-    return callback(event);
-  });
+  if (!domainEventSubscribers[type]) {
+    domainEventSubscribers[type] = [];
+  }
+  domainEventSubscribers[type].push(callback);
 };
 
 const firestore = firebaseApp.firestore();
@@ -32,45 +43,29 @@ const { app } = dixit({
   subscribeToDomainEvent,
 });
 
-// app.get('/replayLastTurnEvents', (req, res) => {
-//   const { turnId } = req.query;
-//   firebaseApp
-//     .firestore()
-//     .collection('turns')
-//     .limit(10000)
-//     .get()
-//     .then(snapshot => {
-//       const turns = [];
-//       snapshot.forEach(doc =>
-//         turns.push(
-//           Object.fromEntries(
-//             Object.entries(doc.data()).map(([timestamp, jsonEvent]) => [timestamp, JSON.parse(jsonEvent)])
-//           )
-//         )
-//       );
-//       fs.writeFile('./turns.json', JSON.stringify(turns, null, 2), (err, _) => {
-//         res.json(turns);
-//       });
-//       // if (!doc.exists) {
-//       //   res.status(500).send('Turn not found');
-//       // }
-//       // const events = Object.entries(doc.data());
-//       // events.sort(([a], [b]) => parseInt(a, 10) - parseInt(b, 10));
-
-//       // const parsedEvents = events.map(([_, e]) => JSON.parse(e));
-//       // console.log(JSON.stringify(parsedEvents, null, 2));
-//       // const state = parsedEvents.reduce(turnReducer, undefined);
-//       // // const [, event] = events[events.length - 1];
-//       // // dispatchDomainEvents([events[events.length - 1]]);
-//       // res.json(state);
-//     });
-// });
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 exports.api = functions
   .runWith({
     timeoutSeconds: 60,
     memory: '1GB',
   })
   .https.onRequest(app);
+
+exports.processEvent = functions
+  .runWith({
+    timeoutSeconds: 60,
+    memory: '1GB',
+  })
+  .pubsub.topic(TOPIC_NAME)
+  .onPublish(message => {
+    // Décodage du message Pub/Sub
+    const event = JSON.parse(Buffer.from(message.data, 'base64').toString());
+    console.log('Événement reçu:', event);
+
+    // Recherche et appel du callback associé au type d'événement
+    const callbacks = domainEventSubscribers[event.type];
+    if (callbacks) {
+      callbacks.forEach(cb => cb(event));
+    } else {
+      console.warn("Aucun subscriber pour l'événement de type:", event.type);
+    }
+  });
